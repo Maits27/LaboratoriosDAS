06499aa5a4aa6ae7675cefa1bbce7320fdb20d00
Maits27 <maitiurlo@gmail.com>
2024-02-04 19:39:52 +0100

    l2 p2

--- L2/app/src/main/java/com/example/l2/MainActivity.kt
+++ L2/app/src/main/java/com/example/l2/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.example.l2
 
-import android.content.res.Configuration
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,15 +17,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import com.example.l2.ui.theme.L2Theme
-import androidx.compose.runtime.*
-import java.util.*
+import java.util.Locale
 
 
 enum class AppLanguage(val language: String, val code: String) {
@@ -33,13 +33,13 @@ enum class AppLanguage(val language: String, val code: String) {
     ES("Español", "es"),
 }
 enum class AppLifecycleState {
-    OnCreate,
     OnStart,
+    OnCreate,
     OnResume,
-    OnRestart,
     OnPause,
     OnStop,
-    OnDestroy
+    OnDestroy,
+    OnRestart
 }
 class ViewModel2 : ViewModel() {
     var cambios by mutableStateOf(0) // Delega los setters y getters a mutable state of
@@ -52,13 +52,15 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel2.transitionedStatesCounter[AppLifecycleState.OnCreate] =
+            viewModel2.transitionedStatesCounter[AppLifecycleState.OnCreate]?.plus(1) ?: 1
         setContent {
             L2Theme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = Color.White
                 ) {
-                    Labo2(cambios = viewModel2.cambios)
+                    Labo2(cambios = viewModel2.cambios, contador = viewModel2.transitionedStatesCounter)
                 }
             }
         }
@@ -67,39 +69,60 @@ class MainActivity : ComponentActivity() {
     override fun onPause() {
         super.onPause()
         viewModel2.cambios++
-        viewModel2.transitionedStatesCounter[AppLifecycleState.OnPause] = viewModel2.transitionedStatesCounter[AppLifecycleState.OnPause]?.plus(1) ?: 1
+        viewModel2.transitionedStatesCounter[AppLifecycleState.OnPause] =
+            viewModel2.transitionedStatesCounter[AppLifecycleState.OnPause]?.plus(1) ?: 1
     }
 
     override fun onStart() {
         super.onStart()
-        viewModel2.transitionedStatesCounter[AppLifecycleState.OnStart] = viewModel2.transitionedStatesCounter[AppLifecycleState.OnStart]?.plus(1) ?: 1
+        viewModel2.transitionedStatesCounter[AppLifecycleState.OnStart] =
+            viewModel2.transitionedStatesCounter[AppLifecycleState.OnStart]?.plus(1) ?: 1
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        viewModel2.transitionedStatesCounter[AppLifecycleState.OnDestroy] = viewModel2.transitionedStatesCounter[AppLifecycleState.OnDestroy]?.plus(1) ?: 1
+        viewModel2.transitionedStatesCounter[AppLifecycleState.OnDestroy] =
+            viewModel2.transitionedStatesCounter[AppLifecycleState.OnDestroy]?.plus(1) ?: 1
     }
 
     override fun onStop() {
         super.onStop()
-        viewModel2.transitionedStatesCounter[AppLifecycleState.OnStop] = viewModel2.transitionedStatesCounter[AppLifecycleState.OnStop]?.plus(1) ?: 1
+        viewModel2.transitionedStatesCounter[AppLifecycleState.OnStop] =
+            viewModel2.transitionedStatesCounter[AppLifecycleState.OnStop]?.plus(1) ?: 1
     }
-    /*
-    override fun onConfigurationChanged(newConfig: Configuration) { //Stop
-        super.onConfigurationChanged(newConfig)
-        viewModel2.cambios++
-        Log.d("MainActivity", "onConfigurationChanged called!!!!!!!") // Agregar este log
+
+    override fun onRestart() {
+        super.onRestart()
+        viewModel2.transitionedStatesCounter[AppLifecycleState.OnRestart] =
+            viewModel2.transitionedStatesCounter[AppLifecycleState.OnRestart]?.plus(1) ?: 1
     }
-     */
+
+    override fun onResume() {
+        super.onResume()
+        viewModel2.transitionedStatesCounter[AppLifecycleState.OnResume] =
+            viewModel2.transitionedStatesCounter[AppLifecycleState.OnResume]?.plus(1) ?: 1
+    }
+
+
 }
 
 @Composable
-fun Labo2(cambios: Int, modifier: Modifier = Modifier) {
+fun Labo2(cambios: Int, contador: Map<AppLifecycleState, Int>,  modifier: Modifier = Modifier) {
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Log.d("MainActivity", " L2!!!!!!!!!!!!") // Agregar este log
-        Text(text = "Ha habido $cambios cambios en la pantalla.")
+        Text(text = stringResource(R.string.contador_mensaje, cambios))
+        Column (
+            Modifier
+                .wrapContentSize()
+                .background(color = Color.LightGray)
+                .align(alignment = Alignment.CenterHorizontally)
+                .padding(16.dp)
+        ) {
+            for (state in AppLifecycleState.values()) {
+                Text(text = stringResource(R.string.estado_mensaje, state, contador[state]?:0), Modifier.padding(top = 5.dp, bottom = 5.dp))
+            }
+        }
     }
 }
