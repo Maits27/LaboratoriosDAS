9fbd4c9bfaa24da6ced69ac636a0f6e5d79f798e
Maits27 <maitiurlo@gmail.com>
2024-02-04 20:36:52 +0100

    l2 fin

--- L2/app/src/main/java/com/example/l2/MainActivity.kt
+++ L2/app/src/main/java/com/example/l2/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.l2
 
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,12 +10,15 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
@@ -21,8 +27,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat.recreate
 import androidx.lifecycle.ViewModel
 import com.example.l2.ui.theme.L2Theme
 import java.util.Locale
@@ -31,6 +39,7 @@ import java.util.Locale
 enum class AppLanguage(val language: String, val code: String) {
     EN("English", "en"),
     ES("Español", "es"),
+    EU("Euskera", "eu")
 }
 enum class AppLifecycleState {
     OnStart,
@@ -45,6 +54,7 @@ class ViewModel2 : ViewModel() {
     var cambios by mutableStateOf(0) // Delega los setters y getters a mutable state of
     val transitionedStates = mutableStateListOf<AppLifecycleState>()
     val transitionedStatesCounter = mutableStateMapOf<AppLifecycleState, Int>()
+    var idioma by mutableStateOf("es")
 }
 
 class MainActivity : ComponentActivity() {
@@ -60,7 +70,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Color.White
                 ) {
-                    Labo2(cambios = viewModel2.cambios, contador = viewModel2.transitionedStatesCounter)
+                    Labo2(
+                        cambios = viewModel2.cambios,
+                        contador = viewModel2.transitionedStatesCounter,
+                        idioma = viewModel2.idioma,
+                        onLanguageChange = {
+                            viewModel2.idioma = it
+                            camcambiarIdioma(viewModel2.idioma)
+                        }
+                    )
                 }
             }
         }
@@ -103,11 +121,18 @@ class MainActivity : ComponentActivity() {
             viewModel2.transitionedStatesCounter[AppLifecycleState.OnResume]?.plus(1) ?: 1
     }
 
+   private fun camcambiarIdioma(codigo: String){
+       resources.configuration.setLocale(Locale(codigo))
+       resources.updateConfiguration(resources.configuration, resources.displayMetrics)
+       
+       resources.configuration.locale = Locale(codigo)
+       resources.updateConfiguration(resources.configuration, resources.displayMetrics)
+    }
 
 }
 
 @Composable
-fun Labo2(cambios: Int, contador: Map<AppLifecycleState, Int>,  modifier: Modifier = Modifier) {
+fun Labo2(cambios: Int, contador: Map<AppLifecycleState, Int>, idioma: String, onLanguageChange:(String)->Unit, modifier: Modifier = Modifier) {
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
@@ -124,5 +149,18 @@ fun Labo2(cambios: Int, contador: Map<AppLifecycleState, Int>,  modifier: Modifi
                 Text(text = stringResource(R.string.estado_mensaje, state, contador[state]?:0), Modifier.padding(top = 5.dp, bottom = 5.dp))
             }
         }
+        Row {
+            for (language in AppLanguage.values()) {
+                if (language.code != idioma.toString()){
+                    Button(onClick = {
+                        onLanguageChange(language.code)
+                        // Cambia el idioma al pulsar el botón
+                    }) {
+                        Text(text = "$language")
+                    }
+                }
+            }
+        }
     }
 }
+
