61fb5b5b5cf8e905efe9258541406dff3c08b7fe
Maits27 <maitiurlo@gmail.com>
2024-02-08 15:04:52 +0100

    L3-no conectan

---
+++ L3/app/src/main/java/com/example/l3/DataActivity.kt
@@ -0,0 +1,68 @@
+package com.example.l3
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.l3.ui.theme.L3Theme
+import com.example.l3.MainActivity
+class DataActivity : ComponentActivity() {
+    private val appViewModel: AppViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("DataActivity", "Dataaa")
+        setContent {
+            L3Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ItemWindow(this, viewModel = appViewModel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ItemWindow(activity: ComponentActivity, viewModel: AppViewModel,  modifier: Modifier = Modifier) {
+    Column (Modifier.wrapContentSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = viewModel.nuevaTarea,
+            onValueChange = {viewModel.nuevaTarea = it},
+            label = { Text(text = "Nuevo TODO")},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        Button(onClick = { viewModel.todoList.add(viewModel.nuevaTarea) }) {
+            Text(text = stringResource(id = R.string.done))
+        }
+
+    }
+}
+
--- L3/app/src/main/java/com/example/l3/MainActivity.kt
+++ L3/app/src/main/java/com/example/l3/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.l3
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
 import com.example.l3.ui.theme.L3Theme
+import java.util.Locale
 
+class AppViewModel : ViewModel() {
+    var nuevaTarea by mutableStateOf("")
+    val todoList = mutableStateListOf<String>()
+}
 class MainActivity : ComponentActivity() {
+    private val appViewModel: AppViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "Me creo")
         setContent {
             L3Theme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +45,58 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TodoList(
+                        this,
+                        viewModel = appViewModel
+                    )
                 }
             }
         }
     }
+    private fun cambiarIdioma(codigo: String){
+        resources.configuration.setLocale(Locale(codigo))
+        resources.updateConfiguration(resources.configuration, resources.displayMetrics)
+
+        resources.configuration.locale = Locale(codigo)
+        resources.updateConfiguration(resources.configuration, resources.displayMetrics)
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TodoList(activity: ComponentActivity, viewModel: AppViewModel, modifier: Modifier = Modifier) {
+    Column (Modifier.wrapContentSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = {
+            Log.d("Button", "Dataaa")
+            val intent = Intent(activity, DataActivity::class.java)
+            activity.startActivity(intent)
+            Log.d("MainActivity", "Dataaa")
+        }) {
+            Text(text = stringResource(id = R.string.add_task))
+        }
+    }
+    LazyColumn (
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Log.d("MainActivity", "Lazy")
+        items(viewModel.todoList.size) { index ->
+            val todo = viewModel.todoList[index]
+            Button(onClick = { /* Handle button click if needed */ }) {
+                Text(text = todo)
+            }
+        }
+        Log.d("MainActivity", "TodoOk")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AppPreview2() {
     L3Theme {
-        Greeting("Android")
+        TodoList(activity = ComponentActivity(), viewModel = AppViewModel())
     }
 }
